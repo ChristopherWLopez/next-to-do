872b9d156e2a89cea0e30ed2a042bd5ccf2bb710
christopher lopez <christopherwlopez89@gmail.com>
2024-03-27 18:45:57 -0700

    async function main component and save todo list into prisma database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
-export default function Home(){
-  return <h1>Hi</h1>
+import Link from "next/link";
+import { Prisma, prisma } from "@/db";
+
+export default async function Home(){
+
+  const todos = await prisma.todo.findMany()
+
+  return <>
+  <header className="flex justify-between items-center mb-4">
+    <h1 className="text-2xl">Todos</h1>
+    <Link 
+      className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+    href="/new">New</Link>
+  </header>
+  <ul>
+    {todos.map(todo =>(
+      <li key={todo.id}>{todo.title}</li>
+    ))}
+  </ul>
+  </>
 }
